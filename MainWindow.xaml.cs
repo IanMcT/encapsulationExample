a40099fa01d46c76b4a3d1f0d20d746778adce3a
IanMcT <ian.mctavish@tldsb.on.ca>
2019-03-18 05:04:12 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace encapsulationExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bankAccount b = new bankAccount("123456");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            double amount;
+            double.TryParse(txtDollarAmount.Text, out amount);
+            if ((bool)rbDeposit.IsChecked)
+            {
+                if (b.Deposit(amount, txtPassword.Password))
+                {
+                    lblOutput.Content = b.ToString();
+                }
+                else
+                {
+                    lblOutput.Content = "Deposit did not go through.  Balance is: " + b.ToString();
+                }
+            }
+            else if ((bool)rbWithdraw.IsChecked)
+            {
+                if (b.Withdrawal(amount, txtPassword.Password))
+                {
+                    lblOutput.Content = b.ToString();
+                }
+                else
+                {
+                    lblOutput.Content = "Withdrawal did not go through.  Balance is: " + b.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ bankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulationExample
+{
+    class bankAccount
+    {
+        private string password;
+        private double balance;
+
+        public bankAccount()
+        {
+            balance = 0;
+        }
+
+        public bankAccount(string p)
+        {
+            password = p;
+            balance = 0;
+        }
+
+        public bool Deposit(double amount, string pword)
+        {
+            if (pword == password &&
+                amount > 0)
+            {
+                balance += amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Withdrawal(double amount, string pword)
+        {
+            if (pword == password
+                && (balance - amount) >= 0)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override string ToString()
+        {
+            return balance.ToString("$0.00");
+        }
+    }
+}
